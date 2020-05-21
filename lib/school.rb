44175ba167086365b8fc2719783c0705b57659ee
Phil Humphrey <phumphrey588@yahoo.com>
2020-05-21 03:52:46 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,49 @@
-# code here!
+# # code here!
+# class AutoPlant
+  
+#   take_inventory
+# #   paint_cars
+  
+#   class School
+   
+    
+#     def initialize(school)
+#       @school = school
+#       @roster = {}
+#     end
+    
+#   def add_student(name, grade)
+#     @name = name
+#     @grade = grade
+#     @roster 
+#   end
+# end
+
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end
